88b13514265049e134d0ea1c898b9d2da35afbc3
Baptiste Manson <baptiste.manson@inovia.fr>
2017-11-12 10:00:55 -0800

    Can select with clicking on the game image.

--- src/Checkboxes.js
+++ src/Checkboxes.js
@@ -4,8 +4,11 @@ export default class Checkboxes extends Component {
 	render() {
 		const { availableOptions, toggle, isChecked } = this.props;
 		const checkboxes = Object.keys(availableOptions).map((option) => (
-			<div className="pa2 tc flex flex-column items-center w-10 justify-end">
-				<img className=" db-ns dn" src={availableOptions[option].img} />
+			<div
+				className="pa2 tc flex flex-column items-center w-10 justify-end pointer"
+				onClick={() => toggle(availableOptions[option].id)}
+			>
+				<img className="db-ns dn" src={availableOptions[option].img} />
 				<span className="f7">{option}</span>
 				<input
 					className=""
